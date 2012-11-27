aa6527befaf1adfae6ebc4e5cffcfaf6ef9f27b8
panmari <smmuzi@gmail.com>
2012-11-27 15:27:47 +0100

    added ballwall

---
+++ BallWall/src/ph/sm/BallWall.java
@@ -0,0 +1,21 @@
+// BallWall.java
+
+package ph.sm;
+
+import ch.aplu.android.*;
+
+public class BallWall extends GameGrid 
+{
+  
+  public BallWall()
+  {
+    super(10, 10, 0, RED);
+  }
+
+  public void main()
+  {
+    showToast("BallWall started");
+  }
+}
+
+
