c51d68130bdbd2db5a7172d66da33be873308a5e
panmari <smmuzi@gmail.com>
2013-03-26 13:33:19 +0100

    added hexagon sprite and added possibility to turn it by mouse

---
+++ Jewels/src/ph/sm/jewels/Hexagon.java
@@ -0,0 +1,42 @@
+package ph.sm.jewels;
+
+import android.graphics.Point;
+import ch.aplu.android.Actor;
+import ch.aplu.android.GGPanel;
+import ch.aplu.android.GGTouch;
+import ch.aplu.android.GGTouchListener;
+import ch.aplu.android.L;
+import ch.aplu.android.PointD;
+
+public class Hexagon extends Actor implements GGTouchListener {
+	
+	private GGPanel p;
+	private int angle = 0;
+	private final int PER_PERIOD_ANGLE = 10;
+
+	public Hexagon(GGPanel p) {
+		super(true, "hexagon_transparent");
+		this.p = p;		
+	}
+
+	public void act() {
+		turn(angle);
+	}
+	
+	@Override
+	public boolean touchEvent(GGTouch touch) {
+		if (touch.getEvent() == GGTouch.press) { // turn on rotating state
+			PointD userPoint = p.toUserPoint(new Point(touch.getX(), touch.getY()));
+			L.d(""+ userPoint);
+			if (userPoint.x > 0) //touch on the right side of display
+				angle = PER_PERIOD_ANGLE;
+			else angle = -PER_PERIOD_ANGLE;
+		} // deactivate rotation on release
+		else {
+			angle = 0;
+		}
+		return true;
+	}
+	
+	
+}
--- Jewels/src/ph/sm/jewels/JewelsGame.java
+++ Jewels/src/ph/sm/jewels/JewelsGame.java
@@ -1,15 +1,27 @@
 package ph.sm.jewels;
 
+import ch.aplu.android.GGPanel;
+import ch.aplu.android.GGPushButton;
 import ch.aplu.android.GGStatusBar;
+import ch.aplu.android.GGTouch;
 import ch.aplu.android.GameGrid;
+import ch.aplu.android.PointD;
 
 public class JewelsGame extends GameGrid {
 	
 	private GGStatusBar status;
+	private GGPanel p;
 
 	public void main() {
+		getBg().clear(WHITE);
+		p = getPanel(-10, 10, 0.5);
+		Hexagon hexagon = new Hexagon(p);
+		addActor(hexagon, toLocation(p.toPixelPoint(new PointD(0,0))));
+		setSimulationPeriod(30);
 		doRun();
 		status.setText("This is how it begins");
+		addTouchListener(hexagon, GGTouch.press | GGTouch.release);
+		addActor( new GGPushButton("Test"), toLocation(p.toPixelPoint(new PointD(0,0))));
 	}
 	
 	public JewelsGame() {
