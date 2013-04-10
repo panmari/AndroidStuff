cd83062381b787c1777c84ac53000d944df385ce
panmari <smmuzi@gmail.com>
2013-04-10 22:26:23 +0200

    fixed bug with crashing every few starts, some improvements to reset
    but reset still doesnt work

--- Jewels/src/ph/sm/jewels/JewelsGame.java
+++ Jewels/src/ph/sm/jewels/JewelsGame.java
@@ -1,6 +1,5 @@
 package ph.sm.jewels;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 
 import ch.aplu.android.Actor;
@@ -18,7 +17,8 @@ public class JewelsGame extends GameGrid implements GGActorCollisionListener {
 	private GGStatusBar status;
 	private Hexagon hexagon;
 	private GGPanel p;
-	private LinkedList<Jewel> jewels = new LinkedList<Jewel>();
+	private LinkedList<Jewel> available_jewels = new LinkedList<Jewel>();
+	private LinkedList<Jewel> all_jewels = new LinkedList<Jewel>();
 	private HealthPointBar hpBar;
 	private int score;
 
@@ -36,38 +36,44 @@ public class JewelsGame extends GameGrid implements GGActorCollisionListener {
 		PointD hexagonSpawnPoint = new PointD(0,0);
 		addActorNoRefresh(hexagon, toLocation(p.toPixelPoint(hexagonSpawnPoint)));
 		for (int i = 1; i < 10; i++) {
-			Jewel j = new Jewel(jewels);
+			Jewel j = new Jewel(available_jewels);
+			all_jewels.add(j);
 			//jewel is added to the jewels list in reset() when added to gamegrid
 			addActorNoRefresh(j, new Location(-100, -100)); //out of sight
 			hexagon.addCollisionActor(j);
 		}
 		hexagon.addActorCollisionListener(this);
 		setSimulationPeriod(30);
-		doRun();
 		setPaintOrder(Hexagon.class, Jewel.class);
 		status.setText("Tap the screen to the right/left to turn Hexa-Pacman!");
 		addTouchListener(hexagon, GGTouch.press | GGTouch.release);
 		hpBar = new HealthPointBar(p, 50);
+		doRun();
 	}
 	
 	public void act() {
 		if (hpBar.isGameOver()) {
 			doPause();
-			showToast("Game Over!");
+			showToast("Game Over, tap screen to reset");
 		}
-		if (!jewels.isEmpty() && Math.random() < 0.05 ) {
+		if (!available_jewels.isEmpty() && Math.random() < 0.05 ) {
 			GGVector v = new GGVector(10, 10);
 			v.rotate(Math.random()*Math.PI*2);
 			PointD spawnPoint = new PointD(v);
-			Actor spawningJewel = jewels.pollFirst();
+			Actor spawningJewel = available_jewels.pollFirst();
 			spawningJewel.setLocation(toLocation(p.toPixelPoint(spawnPoint)));
 			spawningJewel.setDirection(spawningJewel.getLocation().getDirectionTo(hexagon.getLocation()));
 			spawningJewel.setActEnabled(true);
 		}
 	}
 	
-	//not sure if this called before or after reset of reset of actors
+	/**
+	 * Since this is called last by @doReset() (after actors @reset()), we need
+	 * to painfully set the @available_jewels manually, so no jewels turn up
+	 * twice in this list.
+	 */
 	public void reset() {
+		available_jewels = new LinkedList<Jewel>(all_jewels);
 		score = 0;
 		hpBar.setHealth(50);
 		doRun();
