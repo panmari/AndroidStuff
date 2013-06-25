4f14d2e4e6d0ef3c0dc92705fb280698f889bda8
panmari <smmuzi@gmail.com>
2013-06-25 17:03:23 +0200

    faster drawing by checking first if stable

--- Mandelbrot/src/ph/sm/mandelbrot/Mandelbrot.java
+++ Mandelbrot/src/ph/sm/mandelbrot/Mandelbrot.java
@@ -10,12 +10,13 @@ import ch.aplu.android.Location;
 
 public class Mandelbrot extends GameGrid implements GGTouchListener{
 	
-	private final double RADIUS_SQUARE = Math.pow(2, 2);
+	private final double RADIUS_SQUARE = 2*2;
 	private int maxIterations = 50;
 	GGPanel p;
 	private Point start, end;
+	
 	public Mandelbrot() {
-		super(windowZoom(600));
+		super(true, windowZoom(600));
 	}
 	
 	public void main() {
@@ -28,8 +29,8 @@ public class Mandelbrot extends GameGrid implements GGTouchListener{
 		p = getPanel(Math.min(xmin, xmax), Math.max(xmin, xmax), 
 						Math.min(ymin, ymax), Math.max(ymin, ymax));
 		p.setAutoRefreshEnabled(false);
-		//setTitle(String.format("Mandelbrot -- xmin: %.3f xmax: %.3f ymin: %.3f ymax: %.3f", 
-		//		p.getXmin(), p.getXmax(), p.getYmin(), p.getYmax()));
+		setTitle(String.format("Mandelbrot -- xmin: %.3f xmax: %.3f ymin: %.3f ymax: %.3f", 
+				p.getXmin(), p.getXmax(), p.getYmin(), p.getYmax()));
 		
 		for (int xPixel = 0; xPixel < getNbHorzPix(); xPixel++) {
 			for(int yPixel = 0; yPixel < getNbVertPix(); yPixel++) {
@@ -43,8 +44,13 @@ public class Mandelbrot extends GameGrid implements GGTouchListener{
 			if (xPixel % 100 == 0) // refresh every 50 columns
 				refresh();
 		}
+		refresh();
 	}
 
+	/**
+	 * @param iterCount a number between 0 and maxIterations.
+	 * @return a color, that belongs to this number of iterations.
+	 */
 	private int getColorForIterCount(int iterCount) {
 		if (iterCount >= maxIterations)
 			return Color.BLACK;
@@ -55,6 +61,8 @@ public class Mandelbrot extends GameGrid implements GGTouchListener{
 
 
 	private int getIterCount(double x_p, double y_p) {
+		if (isStable(x_p, y_p))
+			return maxIterations;
 		double x = 0, y = 0;
 		int iter = 0;
 		while (isInCircle(x, y) && iter < maxIterations) {
@@ -67,8 +75,16 @@ public class Mandelbrot extends GameGrid implements GGTouchListener{
 		return iter;
 	}
 
-
-
+	/**
+	 * Returns true, if the given points are inside the cardioid (first expression)
+	 * or inside the period 2 bulb (second expression). These values are considered
+	 * stable, since they'll fulfill insideCircle for every iteration step.
+	 */
+	private boolean isStable(double x, double y) {
+		double p = Math.sqrt(Math.pow(x - 1/4f, 2) + y*y);
+		return x < p - 2*(p*p) + 1/4f || (x + 1)*(x + 1) + y*y < 1/16f;
+	}
+	
 	private boolean isInCircle(double x, double y) {
 		return Math.pow(x, 2) + Math.pow(y, 2) < RADIUS_SQUARE;
 	}
@@ -90,7 +106,6 @@ public class Mandelbrot extends GameGrid implements GGTouchListener{
 		case GGTouch.drag:
 			Point current = new Point(l.x, l.y);
 			end = current;
-			getBg().drawRectangle(start, end);
 			break;
 		case GGTouch.release:
 			draw(p.toUserX(start.x), p.toUserX(end.x), p.toUserY(start.y), p.toUserY(end.y));
